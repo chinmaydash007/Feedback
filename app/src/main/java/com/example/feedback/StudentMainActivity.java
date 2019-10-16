70b3427d2022b2331903fd498c00893910563a1a
Chinmay <chinmaydash11@gmail.com>
2019-10-16 23:07:52 +0530

    Working on student side

--- app/src/main/java/com/example/feedback/StudentMainActivity.java
+++ app/src/main/java/com/example/feedback/StudentMainActivity.java
@@ -1,14 +1,17 @@
 package com.example.feedback;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import com.example.feedback.Model.TeacherDetails;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,10 +19,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class StudentMainActivity extends AppCompatActivity {
-FirebaseAuth firebaseAuth;
-Toolbar toolbar;
-DatabaseReference studentRef;
+    FirebaseAuth firebaseAuth;
+    Toolbar toolbar;
+    DatabaseReference studentRef;
+    DatabaseReference feedbackRef;
+    DatabaseReference teacherRef;
+
+    String stduent_index;
+    ArrayList<String> teacherinsidefeedbacklist;
+    ArrayList<TeacherDetails> teacherDetailsArrayList;
+
 
 
 
@@ -27,14 +39,19 @@ DatabaseReference studentRef;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
-        firebaseAuth=FirebaseAuth.getInstance();
-        toolbar=findViewById(R.id.toolbar);
+        firebaseAuth = FirebaseAuth.getInstance();
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
 
-        if(firebaseAuth.getCurrentUser()==null){
-            Intent intent=new Intent(StudentMainActivity.this,LoginActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        teacherinsidefeedbacklist = new ArrayList<>();
+        teacherDetailsArrayList=new ArrayList<>();
+
+
+
+        if (firebaseAuth.getCurrentUser() == null) {
+            Intent intent = new Intent(StudentMainActivity.this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
         }
@@ -43,15 +60,21 @@ DatabaseReference studentRef;
     @Override
     protected void onStart() {
         super.onStart();
-        studentRef= FirebaseDatabase.getInstance().getReference().child("Users").child("Student");
+        studentRef = FirebaseDatabase.getInstance().getReference().child("Users").child("Student");
+        teacherRef = FirebaseDatabase.getInstance().getReference().child("Users").child("Teacher");
+        feedbackRef = FirebaseDatabase.getInstance().getReference().child("Feedback");
+
+
         studentRef.child(firebaseAuth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(!dataSnapshot.hasChild("student_full_name")){
-                    Intent intent=new Intent(StudentMainActivity.this,Student_profile_activity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                if (!dataSnapshot.hasChild("student_full_name")) {
+                    Intent intent = new Intent(StudentMainActivity.this, Student_profile_activity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                     finish();
+                } else {
+                    fetchStudentIndex();
                 }
             }
 
@@ -63,15 +86,74 @@ DatabaseReference studentRef;
 
     }
 
+    private void fetchStudentIndex() {
+        studentRef.child(firebaseAuth.getCurrentUser().getUid()).child("student_index").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null) {
+                    stduent_index = "" + dataSnapshot.getValue();
+                    fetchTeacherChoosenforFeedback(stduent_index);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void fetchTeacherChoosenforFeedback(String stduent_index) {
+        feedbackRef.child(stduent_index).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                        teacherinsidefeedbacklist.add(dataSnapshot1.getKey());
+
+
+                    }
+                }
+                addTeacherDetailstoTeacherDetailsArrayList(teacherinsidefeedbacklist);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addTeacherDetailstoTeacherDetailsArrayList(final ArrayList<String> teacherinsidefeedbacklist) {
+        teacherRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                    TeacherDetails teacherDetails=dataSnapshot1.getValue(TeacherDetails.class);
+                    if(teacherinsidefeedbacklist.contains(teacherDetails.getUid())){
+                        teacherDetailsArrayList.add(teacherDetails);
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.student_menu,menu);
+        getMenuInflater().inflate(R.menu.student_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.logout:
                 logout();
                 return true;
@@ -86,13 +168,14 @@ DatabaseReference studentRef;
     private void logout() {
 
         firebaseAuth.signOut();
-        Intent intent=new Intent(StudentMainActivity.this,LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        Intent intent = new Intent(StudentMainActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
     }
-    private void openStudentProfile(){
-        Intent intent=new Intent(StudentMainActivity.this,Student_profile_activity.class);
+
+    private void openStudentProfile() {
+        Intent intent = new Intent(StudentMainActivity.this, Student_profile_activity.class);
         startActivity(intent);
     }
 }
